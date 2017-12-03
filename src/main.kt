827fd3a877b9f4bfa6871f4e217a8774477d4a83
koalakoker <koalakoker@gmail.com>
2017-12-03 09:20:49 +0100

    Implementing Hash Table

--- src/main.kt
+++ src/main.kt
@@ -2,11 +2,24 @@ import java.io.File
 
 fun main(args: Array<String>)
 {
-    val root : String = "c:\\Project\\"
+//    val root : String = "c:\\Project\\"
+    val root : String = "/Users/koalakoker/IdeaProjects"
     val f : FileTreeWalk = File(root).walkTopDown() ;
-    fun elaborate(str : String)
+    var count : Int = 0
+    var size: Int = 0
+    val map = HashMap<String, String>()
+
+    fun elaborate(str : String, path : String)
     {
-        println (str)
+        //println (str)
+        map.put(str, path)
+
+        count++
+        size += str.length
     }
-    f.forEach {elaborate(it.name)}
+
+    f.forEach {elaborate(it.name, it.absolutePath)}
+    println("Total: ${count} - Size ${size}")
+
+    println(map["app.kt"])
 }
